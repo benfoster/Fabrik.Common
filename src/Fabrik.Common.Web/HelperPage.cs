b019f0b4006d15f9adc9bf8b16652d63d592674e
Ben Foster <ben@planetcloud.co.uk>
2012-07-08 14:09:13 +0100

    Added routeextensions and base class for razor helpers that provides access to Mvc Html Helper

---
+++ src/Fabrik.Common.Web/HelperPage.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+using System.Web.Mvc;
+using System.Web.WebPages;
+
+namespace Fabrik.Common.Web
+{
+    /// <summary>
+    /// Exposes the MVC HtmlHelper to Razor helpers instead of the crappy System.Web.WebPages HtmlHelper
+    /// </summary>
+    public class HelperPage : System.Web.WebPages.HelperPage
+    {
+        public static new HtmlHelper Html
+        {
+            get { return ((System.Web.Mvc.WebViewPage)WebPageContext.Current.Page).Html; }
+        }
+
+        public static new UrlHelper Url
+        {
+            get { return ((System.Web.Mvc.WebViewPage)WebPageContext.Current.Page).Url; }
+        }
+
+        public static ViewDataDictionary ViewData
+        {
+            get { return ((System.Web.Mvc.WebViewPage)WebPageContext.Current.Page).ViewData; }
+        }
+    }
+}
---
+++ src/Fabrik.Common.Web/RouteExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Routing;
+
+namespace Fabrik.Common.Web
+{
+    /// <summary>
+    /// Extensions for <see cref="System.Web.Routing.Route"/>.
+    /// </summary>
+    public static class RouteExtensions
+    {
+        /// <summary>
+        /// Provides a fluent way of adding constraints to a route.
+        /// </summary>
+        public static Route WithConstraint(this Route route, string key, IRouteConstraint contraint)
+        {
+            route.Constraints.Add(key, contraint);
+            return route;
+        }
+    }
+}
