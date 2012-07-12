0364a7a27782e272baea8183a28a3702835f09b9
Ben Foster <ben@planetcloud.co.uk>
2012-07-12 12:23:40 +0100

    Added Mandatory validation attribute

---
+++ src/Fabrik.Common.Web/Validation/MandatoryAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace Fabrik.Common.Web
+{
+    /// <summary>
+    /// A validation attribute to ensure that a boolean property is set to true.
+    /// </summary>
+    /// <remarks>
+    /// Supports client-side validation.
+    /// </remarks>
+    public class MandatoryAttribute : ValidationAttribute, IClientValidatable
+    {
+        public override bool IsValid(object value)
+        {
+            return (!(value is bool) || (bool)value);
+        }
+        public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context)
+        {
+            ModelClientValidationRule rule = new ModelClientValidationRule();
+            rule.ErrorMessage = FormatErrorMessage(metadata.GetDisplayName());
+            rule.ValidationType = "mandatory";
+            yield return rule;
+        }
+    }
+}
