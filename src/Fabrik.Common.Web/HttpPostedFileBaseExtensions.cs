51380caabdbec13516cd623ec51b180259fd23b5
Ben Foster <ben@planetcloud.co.uk>
2012-07-18 09:51:59 +0100

    Added extension methods for httppostedfilebase.

---
+++ src/Fabrik.Common.Web/HttpPostedFileBaseExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web;
+
+namespace Fabrik.Common.Web
+{
+    /// <summary>
+    /// Extensions for <see cref="System.Web.HttpPostedFileBase"/>
+    /// </summary>
+    public static class HttpPostedFileBaseExtensions
+    {
+        /// <summary>
+        /// Checks whether an upload is valid
+        /// </summary>
+        public static bool IsValid(this HttpPostedFileBase file)
+        {
+            return file != null && file.ContentLength > 0;
+        }
+
+        /// <summary>
+        /// Validates whether an upload is an image
+        /// </summary>
+        public static bool IsImage(this HttpPostedFileBase file)
+        {
+            var contentType = MimeMapping.GetMimeMapping(file.FileName);
+            return contentType.StartsWith("image", StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
