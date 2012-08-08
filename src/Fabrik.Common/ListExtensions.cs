2c27156389c053944c57043d2d828f93ab92e3b4
Ben Foster <ben@planetcloud.co.uk>
2012-08-08 09:45:24 +0100

    Added extensions for moving items in a List<T>.

---
+++ src/Fabrik.Common/ListExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fabrik.Common
+{
+    /// <summary>
+    /// Extension methods for <see cref="System.Collections.Generic.List{T}"/>
+    /// </summary>
+    public static class ListExtensions
+    {
+        /// <summary>
+        /// Moves the item matching the <paramref name="itemSelector"/> to the end of the <paramref name="list"/>.
+        /// </summary>
+        public static void MoveToEnd<T>(this List<T> list, Predicate<T> itemSelector)
+        {
+            Ensure.Argument.NotNull(list, "list");
+            if (list.Count > 1)
+                list.Move(itemSelector, list.Count - 1);
+        }
+
+        /// <summary>
+        /// Moves the item matching the <paramref name="itemSelector"/> to the beginning of the <paramref name="list"/>.
+        /// </summary>
+        public static void MoveToBeginning<T>(this List<T> list, Predicate<T> itemSelector)
+        {
+            Ensure.Argument.NotNull(list, "list");
+            list.Move(itemSelector, 0);
+        }
+        
+        /// <summary>
+        /// Moves the item matching the <paramref name="itemSelector"/> to the <paramref name="newIndex"/> in the <paramref name="list"/>.
+        /// </summary>
+        public static void Move<T>(this List<T> list, Predicate<T> itemSelector, int newIndex)
+        {
+            Ensure.Argument.NotNull(list, "list");
+            Ensure.Argument.NotNull(itemSelector, "itemSelector");
+            Ensure.Argument.Is(newIndex >= 0, "New index must be greater than or equal to zero.");
+            
+            var currentIndex = list.FindIndex(itemSelector);
+            Ensure.That<ArgumentException>(currentIndex >= 0, "No item was found that matches the specified selector.");
+
+            if (currentIndex == newIndex)
+                return;
+
+            // Copy the item
+            var item = list[currentIndex];
+
+            // Remove the item from the list
+            list.RemoveAt(currentIndex);
+
+            // Finally insert the item at the new index
+            list.Insert(newIndex, item);
+        }
+    }
+}
---
+++ src/Specs/Fabrik.Common.Specs/ListExtensionSpecs.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Machine.Specifications;
+
+namespace Fabrik.Common.Specs
+{
+    [Subject(typeof(ListExtensions), "Move")]
+    public class List_Move
+    {
+        static List<int> list;
+
+        public class When_no_matching_item_is_found
+        {
+            static Exception exception;
+
+            Establish ctx = () => {
+                list = new List<int>();
+            };
+
+            Because of = ()
+                => exception = Catch.Exception(() => list.Move(x => x == 10, 10));
+
+            It Should_throw_an_exception = ()
+                => exception.ShouldBeOfType<ArgumentException>();
+        }
+
+        public class When_new_index_is_higher
+        {
+            Establish ctx = () => {
+                list = new List<int> { 1, 2, 3, 4, 5 };
+            };
+
+            Because of = ()
+                => list.Move(x => x == 3, 3); // shift 3 up one
+
+            It Should_be_moved_to_the_specified_index = () =>
+                {
+                    list[0].ShouldEqual(1);
+                    list[1].ShouldEqual(2);
+                    list[2].ShouldEqual(4);
+                    list[3].ShouldEqual(3);
+                    list[4].ShouldEqual(5);
+                };
+        }
+
+        public class When_new_index_is_lower
+        {
+            Establish ctx = () => {
+                list = new List<int> { 1, 2, 3, 4, 5 };
+            };
+
+            Because of = ()
+                => list.Move(x => x == 4, 2); // shift 4 down one
+
+            It Should_be_moved_to_the_specified_index = () =>
+            {
+                list[0].ShouldEqual(1);
+                list[1].ShouldEqual(2);
+                list[2].ShouldEqual(4);
+                list[3].ShouldEqual(3);
+                list[4].ShouldEqual(5);
+            };
+        }
+
+        // edge cases
+
+        public class When_moving_the_first_item
+        {
+            Establish ctx = () => {
+                list = new List<int> { 1, 2, 3, 4, 5 };
+            };
+
+            Because of = ()
+                => list.Move(x => x == 1, 1); // shift up 1
+
+            It Should_be_moved_to_the_specified_index = () =>
+            {
+                list[0].ShouldEqual(2);
+                list[1].ShouldEqual(1);
+                list[2].ShouldEqual(3);
+                list[3].ShouldEqual(4);
+                list[4].ShouldEqual(5);
+            };
+        }
+
+        public class When_moving_the_last_item
+        {
+            Establish ctx = () => {
+                list = new List<int> { 1, 2, 3, 4, 5 };
+            };
+
+            Because of = ()
+                => list.Move(x => x == 5, 3); // shift down 1
+
+            It Should_be_moved_to_the_specified_index = () =>
+            {
+                list[0].ShouldEqual(1);
+                list[1].ShouldEqual(2);
+                list[2].ShouldEqual(3);
+                list[3].ShouldEqual(5);
+                list[4].ShouldEqual(4);
+            };
+        }
+    }
+
+    [Subject(typeof(ListExtensions), "MoveToEnd")]
+    public class List_MoveToEnd 
+    {
+        static List<int> list;
+        
+        public class When_list_has_more_than_one_item
+        {
+            Establish ctx = () =>
+            {
+                list = new List<int> { 1, 2, 3, 4, 5 };
+            };
+
+            Because of = ()
+                => list.MoveToEnd(x => x == 1); // move to end
+
+            It Should_be_moved_to_the_end_of_the_list = () =>
+            {
+                list[0].ShouldEqual(2);
+                list[1].ShouldEqual(3);
+                list[2].ShouldEqual(4);
+                list[3].ShouldEqual(5);
+                list[4].ShouldEqual(1);
+            };
+        }
+    }
+
+    [Subject(typeof(ListExtensions), "MoveToBeginning")]
+    public class List_MoveToBeginning
+    {
+        static List<int> list;
+
+        public class When_list_has_more_than_one_item
+        {
+            Establish ctx = () =>
+            {
+                list = new List<int> { 1, 2, 3, 4, 5 };
+            };
+
+            Because of = ()
+                => list.MoveToBeginning(x => x == 5); // move to end
+
+            It Should_be_moved_to_the_end_of_the_list = () =>
+            {
+                list[0].ShouldEqual(5);
+                list[1].ShouldEqual(1);
+                list[2].ShouldEqual(2);
+                list[3].ShouldEqual(3);
+                list[4].ShouldEqual(4);
+            };
+        }
+    }
+}
