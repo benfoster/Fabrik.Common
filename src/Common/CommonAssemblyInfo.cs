5fbd141e2b3bf68157b7591377812207bba9d36c
tugberkugurlu <tugberkugurlu@gmail.com>
2012-09-28 18:52:30 +0300

    added CommonAssemblyInfo.cs file and referenced this as linked resource

---
+++ src/Common/CommonAssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCompany("Ben Foster")]
+[assembly: AssemblyCopyright("Copyright © Ben Foster 2012")]
+
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyConfiguration("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0-pre")]
