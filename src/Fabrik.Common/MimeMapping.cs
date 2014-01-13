f266323d0989657da9c23265bc7a4441bc0637e9
Ben Foster <ben@planetcloud.co.uk>
2014-01-13 14:38:07 +0000

    Added mime type mapping for SVG

--- src/Fabrik.Common/MimeMapping.cs
+++ src/Fabrik.Common/MimeMapping.cs
@@ -304,6 +304,7 @@ namespace Fabrik.Common
             {".stl", "application/vnd.ms-pki.stl"}, 
             {".sv4cpio", "application/x-sv4cpio"}, 
             {".sv4crc", "application/x-sv4crc"}, 
+            {".svg", "image/svg+xml"},
             {".swf", "application/x-shockwave-flash"}, 
 
             {".t", "application/x-troff"}, 
