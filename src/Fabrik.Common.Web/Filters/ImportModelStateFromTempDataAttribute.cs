198c2487d72419b73d1a21111a4ec497c5fb14c8
ben.crawford <ben.crawford@softekinc.com>
2014-05-22 15:02:59 -0500

    Changed ImportModelStateFromTempDataAttribute to check if the result is ViewResultBase instead of ViewResult so it includes PartialViewResults like the comment implies.

--- src/Fabrik.Common.Web/Filters/ImportModelStateFromTempDataAttribute.cs
+++ src/Fabrik.Common.Web/Filters/ImportModelStateFromTempDataAttribute.cs
@@ -16,7 +16,7 @@ namespace Fabrik.Common.Web
         public override void OnActionExecuted(ActionExecutedContext filterContext)
         {
             // Only copy from TempData if we are rendering a View/Partial
-            if (filterContext.Result is ViewResult)
+            if (filterContext.Result is ViewResultBase)
             {
                 ImportModelStateFromTempData(filterContext);
             }
