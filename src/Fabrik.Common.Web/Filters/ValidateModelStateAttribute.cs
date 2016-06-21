a20bb9d6c5e8efc301ca3cf892038172944e7b3f
Ali Servet Donmez <asd@pittle.org>
2016-06-21 15:53:09 +0200

    Redirect to a explicit action (& controller)
    ... instead of reading from current route data values.

--- src/Fabrik.Common.Web/Filters/ValidateModelStateAttribute.cs
+++ src/Fabrik.Common.Web/Filters/ValidateModelStateAttribute.cs
@@ -12,6 +12,19 @@ namespace Fabrik.Common.Web
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
     public class ValidateModelStateAttribute : ModelStateTempDataTransfer
     {
+        public ValidateModelStateAttribute() { }
+        
+        public ValidateModelStateAttribute(string actionName)
+        {
+            this.actionName = actionName;
+        }
+        
+        public ValidateModelStateAttribute(string actionName, string controllerName)
+        {
+            this.actionName = actionName;
+            this.controllerName = controllerName;
+        }
+        
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
             if (!filterContext.Controller.ViewData.ModelState.IsValid)
@@ -34,8 +47,20 @@ namespace Fabrik.Common.Web
             // Export ModelState to TempData so it's available on next request
             ExportModelStateToTempData(filterContext);
 
+            var routeValues = new RouteValueDictionary(filterContext.RouteData.Values);
+
+            if (!string.IsNullOrWhiteSpace(this.actionName))
+            {
+                routeValues["action"] = this.actionName;
+            }
+
+            if (!string.IsNullOrWhiteSpace(this.controllerName))
+            {
+                routeValues["controller"] = this.controllerName;
+            }
+
             // redirect back to GET action
-            filterContext.Result = new RedirectToRouteResult(filterContext.RouteData.Values);
+            filterContext.Result = new RedirectToRouteResult(routeValues);
         }
 
         protected virtual void ProcessAjax(ActionExecutingContext filterContext)
