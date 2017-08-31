0985b1c668c80771a8bb908f39fdd2d73c4dc546
Diana Koenraadt <dev7@dianakoenraadt.nl>
2017-08-31 12:44:48 +0200

    Add RadioButtonListFor to HtmlHelperExtensions. (Still has some code duplication, but I can't compile the solution, so I've done the minimal that I know compiles and works.)

--- src/Fabrik.Common.Web/HtmlHelperExtensions.cs
+++ src/Fabrik.Common.Web/HtmlHelperExtensions.cs
@@ -131,6 +131,18 @@ namespace Fabrik.Common.Web
         }
 
         /// <summary>
+        /// Returns a radio button for each of the provided <paramref name="items"/>.
+        /// </summary>
+        public static MvcHtmlString RadioButtonListFor<TModel, TValue>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TValue>> expression, IEnumerable<SelectListItem> items, object htmlAttributes = null)
+        {
+            var listName = ExpressionHelper.GetExpressionText(expression);
+            var metaData = ModelMetadata.FromLambdaExpression(expression, htmlHelper.ViewData);
+
+            items = GetCheckboxListWithDefaultValues(metaData.Model, items);
+            return htmlHelper.RadioButtonList(listName, items, htmlAttributes);
+        }
+
+        /// <summary>
         /// Returns a checkbox for each of the provided <paramref name="items"/>.
         /// </summary>
         public static MvcHtmlString CheckBoxList(this HtmlHelper htmlHelper, string listName, IEnumerable<SelectListItem> items, object htmlAttributes = null)
@@ -148,6 +160,35 @@ namespace Fabrik.Common.Web
                 cb.MergeAttribute("value", item.Value ?? item.Text);
                 if (item.Selected)
                     cb.MergeAttribute("checked", "checked");
+
+                label.InnerHtml = cb.ToString(TagRenderMode.SelfClosing) + item.Text;
+
+                container.InnerHtml += label.ToString();
+            }
+
+            return new MvcHtmlString(container.ToString());
+        }
+
+        /// <summary>
+        /// Returns a radio button for each of the provided <paramref name="items"/>.
+        /// </summary>
+        public static MvcHtmlString RadioButtonList(this HtmlHelper htmlHelper, string listName, IEnumerable<SelectListItem> items, object htmlAttributes = null)
+        {
+            var container = new TagBuilder("div");
+            foreach (var item in items)
+            {
+                var label = new TagBuilder("label");
+                label.MergeAttribute("class", "radio"); // default class
+                label.MergeAttributes(new RouteValueDictionary(htmlAttributes), true);
+
+                var cb = new TagBuilder("input");
+                cb.MergeAttribute("type", "radio");
+                cb.MergeAttribute("name", listName);
+                cb.MergeAttribute("value", item.Value ?? item.Text);
+                if (item.Selected)
+                {
+                    cb.MergeAttribute("checked", "checked");
+                }
 
                 label.InnerHtml = cb.ToString(TagRenderMode.SelfClosing) + item.Text;
 
